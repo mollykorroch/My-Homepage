e4871237a7e916f2538663c282bff52c9630f2a4
Molly Korroch <mollykorroch@gmail.com>
2017-06-21 13:25:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,37 @@ $(document) .ready(function () {
 		$(this).hide();
 	});
 });
+
+//hide all the images, then bring them up one at a time
+
+var current_image = 0;
+var images = ['easton.1', 'easton.2', 'easton.3', 'easton.4', 'easton.5'];
+var slideshow = $('.slide')
+	slideshow.hide();
+	hideImages();
+
+	$('#'+images[current_image]).show();
+
+};
+
+var hideImages = function() {
+	for (var index in images) {
+		var image_name = images[index];
+		$('#'+image_name.hide();
+	}
+
+}
+
+var currentSlide = function(image_index){
+	hideImages();
+	$('#'+images[image_index]).show;
+	current_image = image_index;
+}
+
+var plusSlides = function(arrow){
+	hideImages();
+	var image_index = current_image + arrow;
+	$('#'+images[current_image + arrow]).show();
+	current_image = image_index;
+
+}
